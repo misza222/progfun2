723d36fb14b16e598c4c5b8f3a8843b210952e7c
Michal Pawlowski <misza222@gmail.com>
2018-04-09 11:15:19 +0200

    Example exercise commit

---
+++ example/src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+import example.Lists
+
+object Main extends App {
+  println(s"Max of List(1,23,34) is ${Lists.max(List(1,23,34))}")
+  println(s"Sum of List(1,23,34) is ${Lists.sum(List(1,23,34))}")
+}
